76a751df0f1d29e583f53f9bf1bdbaefa3e83a19
jvsg6 <jvsg6@mail.ru>
2017-11-03 13:14:56 +0300

    bug fixed

--- compareAxis.py
+++ compareAxis.py
@@ -15,10 +15,13 @@ axeRadius = [0.0,0.2,0.4,0.6,0.8,1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0,12.0,1
 			22.0,24.0,26.0,28.0,30.0,32.0,34.0,36.0,38.0,40.0,42.0,44.0,46.0,48.0,50.0,
 		52.0,54.0,56.0,58.0,60.0,62.0,64.0,66.0,68.0,70.0,72.0,74.0,76.0,78.0,80.0,
 			82.0,84.0,86.0,88.0,90.0,92.0,94.0,96.0,98.0,100.0]
+tab=1 #ключ заполнения таблицы. Если 1, то заполняется процентами, если другое число, то ноликами и единичками
 distance=150 #Расстояние при котором точки можно считать совпадающими
 percent=80 #Процент совпадения точек, при котором можносчитать совпадающими оси следа
 pathToAxis="/home/egor/Programs/stat/delete/Axis/"
 axePoints=[]
+
+
 def main():
 	global axePoints
 	length=float(len(axeRadius)-4)
@@ -47,7 +50,7 @@ def main():
 			point = [float(lst2[i].split(" ")[0].replace('\r','').replace(',', '')), float(lst2[i].split(" ")[1].replace('\r','').replace(',', '')) ]
 			axePoints.append(point)
 		k=j #чтобы не пересчитывать все по 2 раза
-		for k in range(j, len(lst)): #перебира по всем функц. (2ой функционал для сравнения)(с чем сравниваем)
+		for k in range(j, len(lst)): #перебира по всем функц. (2ой функционал для сравнения)(с чем сравниваем) от jтого функционал и до конца, чтобы 2 раза не пересчитывать
 
 			if k==j:
 				ws.cell(row=k+2, column=k+2, value=str("//////////////"))
@@ -70,22 +73,20 @@ def main():
 					n=n+1
 					#print "The distance is {:.3f} m.".format(g['s12'])
 			#print str("compare {0} vs {1}".format(str(lst[j]), str(lst[k])))
-			#ws.cell(row=j + 2, column=k + 2, value="{:.1f}%".format(float(n / 56.0 * 100.0)))
-			if float(n/length*100.0)>float(percent):
-				ws.cell(row=j + 2, column=k + 2, value=str("1"))
+			if tab == 1:
+				var="per"
+				ws.cell(row=j + 2, column=k + 2, value="{:.1f}%".format(float(n / 56.0 * 100.0)))
+				ws.cell(row=k + 2, column=j + 2, value="{:.1f}%".format(float(n / 56.0 * 100.0)))
 			else:
-				ws.cell(row=j + 2, column=k + 2, value=str("0"))
+				var="01"
+				if float(n/length*100.0)>float(percent): #симметрично заполняем таблицу относительно диагонали
+					ws.cell(row=j + 2, column=k + 2, value=str("1"))
+					ws.cell(row=k+2, column=j+2, value=str("1"))   #симметрично заполняем таблицу относительно диагонали
+				else:
+					ws.cell(row=j + 2, column=k + 2, value=str("0"))
+					ws.cell(row=k + 2, column=j + 2, value=str("0"))   #симметрично заполняем таблицу относительно диагонали
 
-
-
-	#print axePoints[20][0], axePoints[20][1]
-	#print axePoints2[20][0], axePoints2[20][1]
-	#print float(axePoints[20][0]), float(axePoints[20][1])
-	#print float(axePoints2[20][0]), float(axePoints2[20][1])
-	wb.save(mypath + "/Compare_{0}_{1}.xlsx".format(str(distance), str(percent)))
-
-
-
+	wb.save(mypath + "/Compare_{0}_{1}_{2}.xlsx".format(str(var), str(distance), str(percent)))
 	return
 
 
