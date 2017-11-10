1b064f4b735366ec1f9dc1a155641f35dbe04a8d
jvsg6 <jvsg6@mail.ru>
2017-11-10 12:40:54 +0300

    add max and average value

--- graph.py
+++ graph.py
@@ -12,15 +12,21 @@ axeRadius = [0.0,0.2,0.4,0.6,0.8,1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0,12.0,1
 			82.0,84.0,86.0,88.0,90.0,92.0,94.0,96.0,98.0,100.0, 120.0, 140.0, 160.0, 180.0, 200.0,
 			220.0, 240.0, 260.0, 280.0, 300.0, 320.0, 340.0, 360.0, 380.0, 400.0, 420.0, 440.0,
 			460.0, 480.0, 500.0]
-pathToPointsA="/home/egor/quest/Ru_106/A/Axis/maxPoint_f0.dat"
+aver=0.0
+pathToPointsA="/home/egor/quest/Ru_106/G/Axis/maxPoint_f0.dat"
 pathToPointsB="/home/egor/quest/Ru_106/B/Axis/maxPoint_f0.dat"
 pathToPointsC="/home/egor/quest/Ru_106/C/Axis/maxPoint_f0.dat"
 pathToPointsD="/home/egor/quest/Ru_106/D/Axis/maxPoint_f0.dat"
 pathToPointsE="/home/egor/quest/Ru_106/E/Axis/maxPoint_f0.dat"
 pathToPointsF="/home/egor/quest/Ru_106/F/Axis/maxPoint_f0.dat"
-#pathToPointsG="/home/egor/quest/Ru_106/G/Axis/maxPoint_f0.dat"
+pathToPointsG="/home/egor/quest/Ru_106/G/Axis/maxPoint_f0.dat"
 def save(name='', fmt='png'):
     pwd = os.getcwd()
+    plt.title(name)
+    pathToFolder=pwd+"/pictures/"
+
+    if os.path.isdir(pathToFolder)==False:
+		os.mkdir(pathToFolder)
     iPath = './pictures/{}'.format(fmt)
     if not os.path.exists(iPath):
         os.mkdir(iPath)
@@ -33,6 +39,7 @@ def graph (path):
 	global x
 	global y
 	global fig
+	global aver
 	lst=[]
 	x=np.array(axeRadius)
 	#print(x)
@@ -45,8 +52,10 @@ def graph (path):
 		#print lst[i].split(" ")[2]
 		#print float(lst[i].split(" ")[3])
 		lst2[i] = float(lst[i].split(" ")[3])
+		aver = aver + float(lst[i].split(" ")[3])
 	y=np.array(lst2)
-	
+	#print lst2
+	aver = aver/len(axeRadius)
 	
 	return x, y
 
@@ -54,7 +63,7 @@ def main():
 	global x
 	global y
 	global fig
-	
+	global aver
 	#graph(pathToPointsA)
 	#print lst2
 	#print y
@@ -66,20 +75,23 @@ def main():
 	print (fig.axes)   # Список текущих областей рисования пуст
 	print (type(fig))   # тип объекта Figure
 	plt.grid(True)     #Добавление сетки
+	a=np.max(graph(pathToPointsA)[1])
+	b=np.array([a]*len(graph(pathToPointsA)[1]))
+	c=np.array([aver]*len(graph(pathToPointsA)[1]))
+	print a
 	
-	plt.plot(graph(pathToPointsA)[0], np.log(graph(pathToPointsA)[1]), 
-	         graph(pathToPointsB)[0], np.log(graph(pathToPointsB)[1]),
-		 graph(pathToPointsC)[0], np.log(graph(pathToPointsC)[1]),
-		 graph(pathToPointsD)[0], np.log(graph(pathToPointsD)[1]),
-		 graph(pathToPointsE)[0], np.log(graph(pathToPointsE)[1]),
-		 graph(pathToPointsF)[0], np.log(graph(pathToPointsF)[1])) 
+	plt.plot(graph(pathToPointsA)[0], np.log(graph(pathToPointsA)[1]),
+		 graph(pathToPointsA)[0], np.log(b),
+		 graph(pathToPointsA)[0], np.log(c)) 
+	plt.text(100.0, np.log(a), 'Max Value = {}'.format(a), fontsize=12)
+	plt.text(100.0, np.log(aver), 'Average Value = {}'.format(aver), fontsize=12)
 	print (fig.axes)
 
 	# смотри преамбулу
-	save(name='pic_1_4_1', fmt='pdf')
-	save(name='pic_1_4_1', fmt='png')
+	save(name='pic_G', fmt='jpg')
+	save(name='pic_G', fmt='png')
 
-	plt.show()
+	#plt.show()
 	return
 	
 
